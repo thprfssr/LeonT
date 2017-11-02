54c6e4be1ef2c79bea883fd9bae22d8b7558ba22
Javier Castro <jaca2899@gmail.com>
2017-11-02 00:20:57 -0400

    modified:   doc/Outline.txt

--- test/LagrangeErrorBound.c
+++ test/LagrangeErrorBound.c
@@ -1,17 +1,12 @@
-//This small program is used to calculate the
-//Lagrange Error Bound for the computation of
-//D decimal digits of e. Specifically, it
-//is used to solve the following inequality:
-//ceil(1 + 10^12 * ln(10)) <= \sum_{k=1}^{n+1} floor(ln(k))
-//Here, n is the number of terms in the Taylor
-//series needed to have the first trillion
-//digits be correct. This formula is derived
-//from the Lagrange Error Bound.
-//
-//Ultimately, the program takes as an input
-//the number of decimal digits to which we wish
-//to calculate e, and it outputs the number of
-//terms in the Taylor series needed.
+/*
+ * This program returns the number n of terms
+ * required in order to compute the first D
+ * digits of e.
+ *
+ * We use the inequality
+ * 1 + 10^12 * ln(10) < \sum_{k=0}^n ln(k)
+ * and solve for n.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,26 +30,30 @@ int main(int argc, char **argv)
 		mpz_t D;
 		mpz_init_set_str(D, argv[1], 10);
 
-		//We set this equal to the left side of our inequality
-		double D_double = mpz_get_d(D);
-		double A_double = ceil(1 + D_double * log(10.0));
-		mpz_t A;
-		mpz_init_set_d(A, A_double);
+		//We set GMP Float default precision to 256 bits
+		//in order to counteract round-off errors.
+		mpf_set_default_prec(256);
 
-		//We instantiate a variable, which will hold the left
+		//We set A equal to the left side of the inequality
+		mpf_t A;
+		mpf_init(A);
+
+		//We instantiate sum, which will hold the left
 		//side of the inequality once the loop is finished.
-		mpz_t sum;
-		mpz_init(sum);
+		mpf_t sum;
+		mpf_init(sum);
 
 		//We declare these variables in order to show the
 		//real-time status of the computation.
 		float f;
 
-		mpz_t counter;
-		mpz_init_set_ui(counter, 1);
+		mpz_t i;
+		mpz_init_set_ui(i, 1);
 		//The loop will break when A < sum
 		while(mpz_cmp(A, sum) >= 0)
 		{
+			mpf_add(sum, sum,
+
 			double c = mpz_get_d(counter);
 			//This takes care of the summatory
 			mpz_add_ui(sum, sum, (unsigned long int) floor(log(c)));
