55302503658a7e57ad8b4152a102f9eeca77964e
Javier Castro <jaca2899@gmail.com>
2018-10-29 16:11:04 -0400

    modified:   test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import math
+import datetime
+
+print("Start:", datetime.datetime.now())
 sum = 0
 for i in range(100000000000):
     if i % 1000000 == 0:
         print(i//1000000, "M:\t", sum)
     sum += math.log(i+1)
 
-print(sum)
+print("Final sum:", sum)
+print("End:", datetime.datetime.now())
