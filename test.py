a4c2b12befdc6db482952c70145d212c8735fbbb
Javier Castro <jaca2899@gmail.com>
2018-10-30 12:38:53 -0400

    modified:   test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ value = 1 + D * math.log(10)
 while sum <= value:
     sum += math.log(i)
     if i % 1000000 == 0:
-        print("i =", i, "M\tlnf(i) =", sum)
+        print("i =", i//1000000, "M\tlnf(i) =", sum)
     i += 1
 
 print("The smallest solution to the inequality 1 + D * ln(10) < lnf(n), with D = 1 trillion, is:")
